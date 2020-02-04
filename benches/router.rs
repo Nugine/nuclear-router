caabbac3caa2ee4cc616c09de93bcdc2e272feb6
Nugine <nugine@foxmail.com>
2020-02-05 03:21:34 +0800

    bench: small-routes and large-routes

--- benches/router.rs
+++ benches/router.rs
@@ -9,6 +9,33 @@ fn router_find(c: &mut Criterion) {
         router.insert("/hello/:name", 1);
         b.iter_with_large_drop(|| router.find("/hello/world"))
     });
+
+    group.bench_function("small-routes", |b| {
+        let mut router: Router<usize> = Router::new();
+        router.insert("/posts/:post_id/comments/:id", 1);
+        router.insert("/posts/:post_id/comments", 2);
+        router.insert("/posts/:post_id", 3);
+        router.insert("/posts", 4);
+        router.insert("/comments", 5);
+        router.insert("/comments/:id", 6);
+        b.iter_with_large_drop(|| router.find("/posts/100/comments/200"))
+    });
+
+    group.bench_function("large-routes", |b| {
+        let mut router: Router<usize> = Router::new();
+        let mut pattern = String::new();
+        for i in 0..26 {
+            pattern.push('/');
+            let c = std::char::from_u32('a' as u32 + i).unwrap();
+            pattern.push(c);
+        }
+        for i in 0..512 {
+            let pattern = format!("{}/{}", pattern, i);
+            router.insert(&pattern, i);
+        }
+        pattern.push_str("/128");
+        b.iter_with_large_drop(|| router.find(&pattern))
+    });
 }
 
 fn router_insert(c: &mut Criterion) {
@@ -19,6 +46,24 @@ fn router_insert(c: &mut Criterion) {
             Router::new,
             |router: &mut Router<usize>| {
                 router.insert("/hello/:name", 1);
+            },
+            BatchSize::SmallInput,
+        )
+    });
+
+    group.bench_function("large-route", |b| {
+        let mut pattern = String::new();
+        for i in 0..26 {
+            pattern.push('/');
+            let c = std::char::from_u32('a' as u32 + i).unwrap();
+            pattern.push(c);
+        }
+        pattern.push_str("/128");
+
+        b.iter_batched_ref(
+            Router::new,
+            |router: &mut Router<usize>| {
+                router.insert(&pattern, 1);
             },
             BatchSize::SmallInput,
         )
